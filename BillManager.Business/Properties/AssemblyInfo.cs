66ce47c62b84f3b222fc4e29757c3e27e48a7065
Daniel Santiago Melo Suárez <daniel.melos98@gmail.com>
2022-04-19 19:01:36 -0500

    Ajuste de nombramiento

--- BillManager.Business/Properties/AssemblyInfo.cs
+++ BillManager.Business/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BillManager.Business")]
+[assembly: AssemblyTitle("BillManager.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BillManager.Business")]
+[assembly: AssemblyProduct("BillManager.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
